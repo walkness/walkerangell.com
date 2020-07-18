871b5b4e49d765e51c5bc1a4f38f90dbc36e25f6
Walker Angell <w@lker.co>
2020-07-18 09:03:34 -0500

    port initial configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-react-css-modules-scss-support',
+      options: {
+        localIdentName: '[local]__[hash:base64:5]',
+      },
+    },
+    'gatsby-plugin-postcss',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
