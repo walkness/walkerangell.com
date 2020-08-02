f87acd77e666aac27f0387726de5c4978902438f
Walker Angell <w@lker.co>
2020-08-02 09:45:37 -0500

    add Sentry

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,6 +8,19 @@
 import React from 'react';
 import TypekitLoadedContextProvider from '@/context/typekit_loaded_context';
 
+export const onClientEntry = () => {
+  if (process.env.NODE_ENV === 'production') {
+    import('@sentry/react').then((Sentry) => {
+      Sentry.init({
+        dsn: 'https://e8c83e7128bc4c1fb41c15b88c5a5ed7@o428859.ingest.sentry.io/5374668',
+        environment: process.env.NODE_ENV,
+        release: process.env.AWS_COMMIT_ID,
+        serverName: process.env.AWS_APP_ID,
+      });
+    });
+  }
+};
+
 export const shouldUpdateScroll = ({ routerProps }) => {
   const { location } = routerProps;
   const { userScroll } = location.state || {};
